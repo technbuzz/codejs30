d7b302d5e7fb4ca7011cd27f7eb68680da7cd778
Samiullah Khan <samiullah.khan.1989@gmail.com>
2016-12-20 23:01:17 +0500

    function is ready form needs improvement

--- day2/script.js
+++ day2/script.js
@@ -1,8 +1,10 @@
 const inputs = document.querySelectorAll('.controls input');
 function handleUpdate(){
-    console.log(this);
+    const mu = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.id}`, this.value + mu);
 }
 
 inputs.forEach(function(e){
     e.addEventListener('change', handleUpdate);
+    // e.addEventListener('mousemove', handleUpdate);
 })
