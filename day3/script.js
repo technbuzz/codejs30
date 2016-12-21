a9167996ad6f25ddfa2d7f07f65529509f3f734f
Samiullah Khan <samiullah.khan.1989@gmail.com>
2016-12-21 21:49:14 +0500

    day3 with interactive clock

---
+++ day3/script.js
@@ -0,0 +1,23 @@
+const secondHand = document.querySelector('.seconds');
+const mHand = document.querySelector('.minutes');
+const hHand = document.querySelector('.hours');
+
+function updateClock(){
+    var now = new Date();
+    var seconds = now.getSeconds();
+    var secondsDegree = (seconds/60) * 360;
+    secondHand.style.transform = `rotate(${secondsDegree}deg)`;
+
+    var minutes = now.getMinutes();
+    var minutesDegree = (minutes/60) * 360;
+    mHand.style.transform = `rotate(${minutesDegree}deg)`;
+
+    var hours = now.getHours();
+    var hoursDegree = (hours/12) * 360;
+    hHand.style.transform = `rotate(${hoursDegree}deg)`;
+
+    console.log(seconds);
+    
+}
+
+window.setInterval(updateClock, 1000);
