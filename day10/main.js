528e6e79a2ffdedc8e9969233f7fa93580f9b04d
Samiullah <samiullah.khan.1989@gmail.com>
2017-02-10 20:47:40 +0500

    finished with the day10

--- day10/main.js
+++ day10/main.js
@@ -1,14 +1,61 @@
-var inputs = document.querySelectorAll('input[type="checkbox"]');
+var inputs = document.querySelectorAll('.item input[type="checkbox"]');
 console.log(inputs);
 
+// Highlight the label for checeked inputs
+// inputs.forEach(function(input){
+//   input.addEventListener('change', function(e){
+//     var label = e.target.nextSibling;
+//     debugger;
+//     if(e.target.checked){
+//       label.classList.toggle('hl');
+//     } else {
+//       label.classList.toggle('hl');
+//     } 
+//   })
+// }, true)
+let lastChecked;
+
+//inbetween is a flag variable
+var inbetween = false;
+
+function handleChecked(e){
+  console.log(e);
+  if(e.shiftKey && this.checked){
+    for(var i=0; i<inputs.length; i++){
+      if(inputs[i]==this || inputs[i] == lastChecked){
+        // debugger;
+        inbetween = !inbetween;
+      }
+      if(inbetween){
+        inputs[i].checked = true;
+      }
+    }// for loop
+  }//
+  
+  lastChecked = this;
+}
+
+// Do the shift click magic
 inputs.forEach(function(input){
-  input.addEventListener('change', function(e){
-    var label = e.target.nextSibling;
-    console.log(e.target.checked);
-    if(e.target.checked){
-      label.classList.toggle('hl');
-    } else {
-      label.classList.toggle('hl');
-    }
-  })
+  input.addEventListener('click', handleChecked)
+})
+
+document.getElementById('all').addEventListener('click', function(e){
+  if(e.target.checked){
+    inputs.forEach(function(input){
+      // debugger;
+      input.checked = true;
+    })
+  } else {
+    inputs.forEach(function (input) {
+      // debugger;
+      input.checked = false;
+    })
+  }
+}, true)
+
+
+document.getElementById('lead').addEventListener('click', function(e){
+  console.log(e);
+  
 })
